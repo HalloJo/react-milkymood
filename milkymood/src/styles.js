21c3194cc36fc51d974571a8d4d79cd6a06d6a7f
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-03-14 12:15:20 +0100

    Import Work sans font

--- milkymood/src/styles.js
+++ milkymood/src/styles.js
@@ -2,6 +2,8 @@ import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     body {
+        font-family: 'Work Sans', sans-serif;
+        font-weight: 500;
         color: white;
         text-align: center;
         height: 100vh;
